93d65fd2cf1fa97f302d80d1cd5af8902bbea9f7
Robalcor <roberto.aldaz58@gmail.com>
2025-01-23 19:50:58 -0800

    agregue un grafico de dispercion

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+st.title('Grafica de Barras')
+
 poke_data = pd.read_csv('Pokemon.csv') #leo el database
 hist_botton =st.button('Construir Histograma') #creo el boto para el histograma
+
+
 
 if hist_botton: # al haer clic al boton
     #escribir un mensaje
@@ -14,3 +18,26 @@ if hist_botton: # al haer clic al boton
 
     #mostra el grafico plotly interactibo
     st.plotly_chart(fig_1, use_container_width=True)
+
+#grafico de dispercion
+st.title('Grafico de dispercion')
+
+#lista de tipos
+tlist = ['Grass', 'Fire', 'Water', 'Bug', 'Normal', 'Poison', 'Electric', 'Ground', 'Fairy', 'Fighting', 'Psychic', 'Rock', 'Ghost', 'Ice', 'Dragon', 'Dark', 'Steel', 'Flying']
+
+#lista de estadisticas
+elist = ['HP', 'Attack', 'Defense', 'Sp.Atk', 'Sp.Def', 'Speed', 'Total']
+
+#multiselect de paramatros para la grafica de dispercion
+tipos = st.multiselect('Selecciona tipo', tlist, default='Grass') 
+valx = st.selectbox('seleciona una estaditica', elist, index=0)
+valy = st.selectbox('Seleciona otra estadistica', elist, index=1)
+
+if tipos:
+    valor_t = poke_data[poke_data['Type1'].isin(tipos)]
+else:
+    valor_t = poke_data
+
+
+fig_2 = px.scatter(valor_t, x=valx, y=valy)
+st.plotly_chart(fig_2, use_container_width=True)
